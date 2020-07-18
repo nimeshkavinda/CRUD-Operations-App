d97e4789208ddbe2867dbfd21a0b56a6641f5344
nimeshkavinda <nimeshkavinda13@gmail.com>
2020-07-18 21:39:41 +0530

    Project Initiated

---
+++ SeasideSouthPark/SeasideSouthPark/Login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeasideSouthPark
+{
+    public partial class formLogin : Form
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClose_MouseEnter(object sender, EventArgs e)
+        {
+            btnClose.Size = new Size(21,21);
+        }
+
+        private void btnClose_MouseLeave(object sender, EventArgs e)
+        {
+            btnClose.Size = new Size(20, 20);
+        }
+
+        private void txtUserName_Enter(object sender, EventArgs e)
+        {
+            if (txtUserName.Text == "Username")
+            {
+                txtUserName.Text = "";
+                txtUserName.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            if(txtPassword.Text == "Password")
+            {
+                txtPassword.Text = "";
+                txtPassword.PasswordChar = '*';
+                txtPassword.ForeColor = Color.Black;
+            }
+        }
+    }
+}
